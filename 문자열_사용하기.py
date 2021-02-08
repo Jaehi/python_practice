832529be0f5c9aae4bf91129885941b880d8ab78
jungjaehi <damnyomoonwon1003@gmail.com>
2021-02-08 16:46:19 +0900

    문자열 사용하기

---
+++ 문자열_사용하기.py
@@ -0,0 +1,26 @@
+jeongjaehuiui-MacBookPro:python_practice jungjaehi$ python3
+Python 3.8.7 (v3.8.7:6503f05dd5, Dec 21 2020, 12:45:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> s = '''python is a programming language that lets you work quickly
+... and
+... integrate systems more effectively'''
+>>> print(s)
+python is a programming language that lets you work quickly
+and
+integrate systems more effectively
+>>> s
+'python is a programming language that lets you work quickly\nand\nintegrate systems more effectively'
+>>> hello = 'hello'
+>>> hello
+'hello'
+>>> print(hello)
+hello
+>>> asdf = """Hi my name is "JaeHi"
+... I'm so 'Happy'"""
+>>> asdf
+'Hi my name is "JaeHi"\nI\'m so \'Happy\''
+>>> print(asdf)
+Hi my name is "JaeHi"
+I'm so 'Happy'
+>>> 
